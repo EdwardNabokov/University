b85d59e170d3f547b4f2bae1650b0911fb6541b7
EdwardNabokov <edwardnabokov@gmail.com>
2017-10-01 00:12:55 +0300

    Add ASD work

---
+++ 2nd_year_program/ASD/Prototypes.h
@@ -0,0 +1,18 @@
+#pragma once
+void createTable();
+int selectTable();
+void insertTable();
+void deleteTable();
+void changeTable();
+void searchTable();
+int binarySearch(char *);
+int searchBySurname(char *);
+void sort();
+void outputTable();
+int choice(int, int);
+bool testSymbol(char &);
+bool testString(char *);
+bool testNum(int &);
+bool testRow(int &);
+int getKey(int);
+
---
+++ 2nd_year_program/ASD/Tables.h
@@ -0,0 +1,11 @@
+#pragma once
+struct Data
+{
+	unsigned short day, month, year;
+};
+struct student
+{
+	char *surname;
+	unsigned char index;
+	Data birthday;
+};
