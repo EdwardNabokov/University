8261a6e5399fb1385f5b7c8c7029e070c0e3ed81
enabokov <edwardnabokov@gmail.com>
2017-10-10 16:51:09 +0300

    Add java works

---
+++ 3rd_year_program/Java/solutions/java_Lab-Works/src/com/solutions/labwork2/src/Interface2.java
@@ -0,0 +1,17 @@
+package com.solutions.labwork2.src;
+
+/**
+ * Interface definition called Interface1
+ * with three methods (meth1, meth2, meth3).
+ * It inherites Interface3.
+ *
+ * @author Eduard Nabokov
+ * @version 0.1
+ * @since 2017.10.10
+ */
+
+public interface Interface2 extends Interface3 {
+    public void meth1();
+    public void meth2();
+    public void meth3();
+}
