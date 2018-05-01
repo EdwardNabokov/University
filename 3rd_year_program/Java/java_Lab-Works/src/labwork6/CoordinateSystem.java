0fc1849c5548c6649a4e96db37c7699acf03fc61
enabokov <edwardnabokov@gmail.com>
2018-05-01 13:04:51 +0300

    Drop unnecessary files

--- 3rd_year_program/Java/java_Lab-Works/src/labwork6/CoordinateSystem.java
+++
@@ -1,6 +0,0 @@
-package com.solutions.labwork6;
-
-interface CoordinateSystem {
-    void draw(double a, double b, double c);
-}
-
--- 3rd_year_program/OS/solutions/os_Lab-Work-1/memoryLib.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void *mem_alloc(size_t);
-void *mem_realloc(void*, size_t);
-void mem_free(void *);
-void mergeBlocks();
-void dump();
