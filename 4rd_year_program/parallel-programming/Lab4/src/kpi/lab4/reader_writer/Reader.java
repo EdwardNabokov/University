a5da92fe41d464258ec28d5e8f1b3ba9eac709cb
Eduard Nabokov <edwardnabokov@gmail.com>
2018-12-16 17:33:33 +0200

    Add parallel lab7 (#7)
    * Add lab5 for parallel programming
    
    * Add Lab4
    
    * Implement Tic Tac Toe client/server application

---
+++ 4rd_year_program/parallel-programming/Lab4/src/kpi/lab4/reader_writer/Reader.java
@@ -0,0 +1,26 @@
+package kpi.lab4.reader_writer;
+
+public class Reader extends Thread {
+
+    private String name;
+    private Storage storage;
+
+    Reader(String name, Storage storage) {
+        this.name = name;
+        this.storage = storage;
+    }
+
+    public void run() {
+
+        System.out.println(name + " wants to read");
+
+        String text;
+        try {
+            text = storage.read();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("\n" + name + " read:" + text + "\n");
+    }
+}
