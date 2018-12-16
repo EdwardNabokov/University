a98fcec7535a2ff86e767b66a5e98cc36cad6fbf
enabokov <edwardnabokov@gmail.com>
2018-12-17 01:04:47 +0200

    Implement lab6

---
+++ 4rd_year_program/parallel-programming/Lab6/src/kpi/lab6/CustomThread.java
@@ -0,0 +1,34 @@
+package kpi.lab6;
+
+public class CustomThread extends Thread {
+    Matrix matrix;
+    Matrix vector;
+
+    int rowIndex;
+
+    double result = 0;
+
+    public CustomThread(Matrix matrix, Matrix vector, int rowIndex) {
+        this.matrix = matrix;
+        this.vector = vector;
+
+        this.rowIndex = rowIndex;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    @Override
+    public void run() {
+        if(this.vector.getMatrix().length > 1) {
+            for (int i = 0; i < this.matrix.getMatrix()[0].length; i++) {
+                this.result += this.matrix.getMatrix()[this.rowIndex][i] * this.vector.getMatrix()[i][0];
+            }
+        } else {
+            for (int i = 0; i < this.matrix.getMatrix()[0].length; i++) {
+                this.result += this.vector.getMatrix()[0][i] * this.matrix.getMatrix()[i][this.rowIndex];
+            }
+        }
+    }
+}
