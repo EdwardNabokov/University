ce1ee9bac5ebb5fee811f0863c15ae755478dc06
enabokov <edwardnabokov@gmail.com>
2018-12-18 09:08:30 +0200

    Fixes
    Former-commit-id: 986e0ae37c94630a610d2b0ecef2b124d558a6e0

--- 4rd_year_program/parallel-programming/Lab6/src/kpi/lab6/Lab6.java
+++ 4rd_year_program/parallel-programming/Lab6/src/kpi/lab6/Lab6.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Lab6 {
     static int SIZE = 5000000;
 
-    public static void main(String [] args) throws InterruptedException {
+    public static void main(String [] args) {
         MPI.Init(args);
         double[] vector = new double[SIZE];
         int rank = MPI.COMM_WORLD.Rank();
@@ -18,6 +18,8 @@ public class Lab6 {
         }
 
         double startTime = System.nanoTime();
+
+        // Casting the whole array in parallel
         MPI.COMM_WORLD.Bcast(vector, 0, SIZE, MPI.DOUBLE, 0);
 
         int end = (rank == (size - 1)) ? SIZE : (SIZE / size) * (rank + 1);
@@ -29,12 +31,14 @@ public class Lab6 {
 
         double[] parallelResult = new double[1];
         parallelResult[0] = sum;
+
+        // one of the element of MapReduce module
         MPI.COMM_WORLD.Reduce(parallelResult, 0, parallelResult, 0, 1, MPI.DOUBLE, MPI.SUM, 0);
         if (rank == 0) {
             double parallel_time = System.nanoTime() - startTime;
             parallelResult[0] = Math.sqrt(parallelResult[0]);
-            System.out.format("\tParallel results: %.6f\n", parallelResult[0]);
-            System.out.format("\tParallel time: %.3fs\n", parallel_time / 1e9);
+            System.out.printf("Parallel Summing: %f\n", parallelResult[0]);
+            System.out.printf("Total Time: %fs\n", parallel_time / 1e9);
         }
 
         MPI.Finalize();
