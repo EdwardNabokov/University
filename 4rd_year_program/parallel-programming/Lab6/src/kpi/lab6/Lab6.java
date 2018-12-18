cc1aed52ed67b56c374b426543610cfc2f2320f5
enabokov <edwardnabokov@gmail.com>
2018-12-18 08:49:29 +0200

    Fixes in Lab6

--- 4rd_year_program/parallel-programming/Lab6/src/kpi/lab6/Lab6.java
+++ 4rd_year_program/parallel-programming/Lab6/src/kpi/lab6/Lab6.java
@@ -20,15 +20,14 @@ public class Lab6 {
                 vector[i] = gen.nextDouble();
             }
         }
-        double start_time = System.nanoTime();
 
+        double start_time = System.nanoTime();
         MPI.COMM_WORLD.Bcast(vector, 0, SIZE, MPI.DOUBLE, 0);
 
-        int begin = (SIZE / size) * rank;
         int end = (rank == (size - 1)) ? SIZE : (SIZE / size) * (rank + 1);
-        double sum = 0;
 
-        for (int i = begin; i < end; i++) {
+        double sum = 0;
+        for (int i = (SIZE / size) * rank; i < end; i++) {
             sum += Math.pow(vector[i], 2);
         }
 
@@ -37,48 +36,12 @@ public class Lab6 {
         MPI.COMM_WORLD.Reduce(parallelResult, 0, parallelResult, 0, 1, MPI.DOUBLE, MPI.SUM, 0);
 
         if (rank == 0) {
-            double end_time = System.nanoTime();
-            double parallel_time = end_time - start_time;
+            double parallel_time = System.nanoTime() - start_time;
             parallelResult[0] = Math.sqrt(parallelResult[0]);
             System.out.format("\tParallel results: %.6f\n", parallelResult[0]);
             System.out.format("\tParallel time: %.3fs\n", parallel_time / 1e9);
         }
+
         MPI.Finalize();
-    }
-}
-
-class Ex {
-    public static int SIZE = 300;
-    public static int NUMBER_JOBS = SIZE;
-
-    public static void main(String... args) throws InterruptedException {
-        System.out.println("Matrix");
-        Matrix matrix = new Matrix(SIZE, SIZE);
-        matrix.fill();
-        matrix.print();
-
-        System.out.println("Vector");
-        Matrix vector = new Matrix(SIZE, 1);
-        vector.fill();
-        vector.print();
-
-        CustomThread[] threadArray = new CustomThread[NUMBER_JOBS];
-
-        for(int i = 0; i < NUMBER_JOBS; i++) {
-            threadArray[i] = new CustomThread(matrix, vector, i);
-            threadArray[i].start();
-        }
-
-        for(int i = 0; i < NUMBER_JOBS; i++) {
-            threadArray[i].join();
-        }
-
-        Matrix result = new Matrix(vector.getRows(), vector.getColumns());
-        for(int i = 0; i < NUMBER_JOBS; i++) {
-            result.getMatrix()[i][0] = threadArray[i].getResult();
-        }
-
-        System.out.println("Result");
-        result.print();
     }
 }
